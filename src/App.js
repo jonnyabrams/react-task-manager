d1c674cfa1b5a948081c7ad9d52ae0168bb998f1
Jonny Abrams <jonnyabrams@hotmail.com>
2022-04-30 12:07:32 +0100

    Add message for when there are no tasks

--- src/App.js
+++ src/App.js
@@ -26,8 +26,8 @@ const App = () => {
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete=
-      {deleteTask} />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete=
+      {deleteTask} /> : 'Nothing to see here'}
     </div>
   );
 }
